ecd343a2809594a08b57732284785708bb326eaa
sanghyun Lee <tkdgus1569@gmail.com>
2024-09-06 21:37:32 +0900

    feat: accounts 작업 준비

--- test3/test3/settings.py
+++ test3/test3/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'articles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'kr-ko'
 
 TIME_ZONE = 'UTC'
 
